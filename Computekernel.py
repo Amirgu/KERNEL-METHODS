0ac04ca0ec42eaf4e12bd095f3f60cacbfc36d2f
amine <aminerguig123@gmail.com>
2023-05-03 01:15:28 +0200

    Read_me update

--- Computekernel.py
+++ Computekernel.py
@@ -7,11 +7,15 @@ from algorithms import load_graphs
 from grakel import *
 import time
 
+### Loading :
+
 with open('/home/amine/Graphs1200.pkl','rb') as f:    ###LOAD RECONSTRUCTED SET 
    Gtrain=pickle.load(f)
 
 with open('/home/amine/projet/data/test_data.pkl','rb') as f:         ###LOAD TEST SET
    Gtest=pickle.load(f)
+
+### Concatenating Reconstructed Training set and test set:
 
 G3=[]
 for i in range(len(Gtrain)):
@@ -22,6 +26,9 @@ for i in range(len(Gtest)):
 G = graph_from_networkx(G3, node_labels_tag="labels",edge_labels_tag="labels")
 
 start_time = time.time()
+
+###Computing Kernel Matrix: 
+
 
 tk=WeisfeilerLehmanOptimalAssignment(n_jobs=2, verbose=False, normalize=False, n_iter=3, sparse=False)
 
